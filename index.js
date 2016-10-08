d34a2af1d6c9a55b919e681b9bbae7d6246a1b18
Emil 'Skeen' Madsen <sovende@gmail.com>
2016-10-08 15:27:26 +0200

    Firefox testing-1 code

--- index.js
+++ index.js
@@ -7,18 +7,33 @@ var driver = new webdriver.Builder()
     .forBrowser('firefox')
     .build();
 
-driver.get('localhost:8081');
+driver.get('http://localhost:8081');
+//driver.get('http://google.dk');
+/*
+driver.executeScript("window.open();");
+driver.getAllWindowHandles().then(function(value) { console.log(value); driver.switchTo().window(value[1]) });
+*/
+driver.findElement(By.css("body")).sendKeys(Key.CONTROL +"t");
+driver.switchTo().defaultContent();
+driver.get('http://localhost:8081');
+//driver.get('http://google.dk');
+
+
+//setInterval(function() {}, 1000);
+
+driver.findElement(By.css("body")).sendKeys(Key.CONTROL +"\t");
+driver.switchTo().defaultContent();
 
 // Setup prefix field
 var prefix_field = By.id('prefix');
 driver.wait(until.elementLocated(prefix_field));
-driver.findElement(prefix_field).sendKeys("selenium");
+driver.findElement(prefix_field).sendKeys("firefox_test");
 // Setup timeout field
 var timeout_field = By.id('timeout');
 driver.wait(until.elementLocated(timeout_field));
 driver.findElement(timeout_field).sendKeys(
         Key.chord(Key.CONTROL, "a"),
-        "5");
+        "50");
 // TODO: Pick channel
 // ...
 
@@ -44,6 +59,18 @@ driver.wait(until.elementTextContains(driver.findElement(output_field), "Channel
 var read_button = By.id('reading');
 driver.wait(until.elementLocated(read_button));
 driver.findElement(read_button).click();
+
+driver.findElement(By.css("body")).sendKeys(Key.CONTROL +"\t");
+driver.switchTo().defaultContent();
+
+// Start reading
+var sos_button = By.id('sos');
+driver.wait(until.elementLocated(sos_button));
+driver.findElement(sos_button).click();
+
+driver.findElement(By.css("body")).sendKeys(Key.CONTROL +"\t");
+driver.switchTo().defaultContent();
+
 // Check that reading completes
 driver.wait(until.elementTextContains(driver.findElement(output_field), "Done readings."), 100 * 1000);
 
@@ -56,3 +83,4 @@ driver.wait(until.elementTextContains(driver.findElement(output_field), "Done se
 
 // Shutdown the webdriver
 driver.quit();
+
