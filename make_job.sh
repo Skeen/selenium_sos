6f77cf39d4712575f8e592c1533d7cf919e019f2
Rasmus Mosbech Jensen <20105109@post.au.dk>
2016-11-30 16:03:12 +0100

    jobs are now generated out of order. Metadata descriptions can be provided

--- make_job.sh
+++ make_job.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 if [ "$#" -lt 4 ]; then
-	echo "Expected 4 args: target_file output_name browser channel"
+	echo "Expected 4 args: target_file output_name browser channel" 1>&2
 	exit
 fi
-
 TARGETS=$(cat $1)
 
 SET_NAME="${2}"
@@ -15,13 +14,19 @@ BROWSER="${3}"
 
 AMBIENT="${5}"
 
+META_DESC=""
+
 DIFFICULTY=$(node calibrate.js $BROWSER $CHANNEL)
 
+# Include description if provided
+if [ "$#" -ge 6 ]; then
+	META_DESC="${6}"
+fi
 
-for SITE in ${TARGETS[@]}
+for i in {1..30}
 do
-	for i in {1..30}
+	for SITE in ${TARGETS[@]}
 	do
-		echo "node index.js $BROWSER ${SITE} \"/${SET_NAME}/$(basename ${SITE})_${i}\" $CHANNEL $DIFFICULTY ${AMBIENT:-0}"
+		echo "node index.js $BROWSER ${SITE} \"/${SET_NAME}/$(basename ${SITE})_${i}\" $CHANNEL $DIFFICULTY ${AMBIENT:-0} $META_DESC"
 	done
 done
